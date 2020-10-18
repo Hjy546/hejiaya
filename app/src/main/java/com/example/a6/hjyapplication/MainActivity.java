db9262090585454cf941cf577fa62e51a6ab01d6
zzzz <1556874368@qq.com>
2020-10-18 18:03:33 +0800

    第一次

---
+++ app/src/main/java/com/example/a6/hjyapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.a6.hjyapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView a = findViewById(R.id.a);
+        a.setText("sadadadas\rdsa\ndasda\n dasdsadsa");
+    }
+}
